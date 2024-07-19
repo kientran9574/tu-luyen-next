b6eff676b7a739a2e636d57f17f1760580d9096b
Your Name <you@example.com>
2024-07-19 22:27:23 +0700

    xong

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -1,11 +1,15 @@
+import createUser from "@/lib/actions/user.actions";
+import { WebhookEvent } from "@clerk/nextjs/server";
+import { headers } from "next/headers";
+import { NextResponse } from "next/server";
 import { Webhook } from "svix";
 
 const webhookSecret: string = process.env.WEBHOOK_SECRET || "";
 
 export async function POST(req: Request) {
   const svix_id = req.headers.get("svix-id") ?? "";
-  const svix_timestamp = req.headers.get("svix-timestamp") ?? "";
-  const svix_signature = req.headers.get("svix-signature") ?? "";
+  const svix_timestamp = headers().get("svix-timestamp") ?? "";
+  const svix_signature = headers().get("svix-signature") ?? "";
   if (!process.env.WEBHOOK_SECRET) {
     throw new Error("WEBHOOK is not set");
   }
@@ -13,20 +17,33 @@ export async function POST(req: Request) {
   const body = JSON.stringify(payload);
   const sivx = new Webhook(process.env.WEBHOOK_SECRET);
 
-  let msg;
+  let msg: WebhookEvent;
 
   try {
     msg = sivx.verify(body, {
       "svix-id": svix_id,
       "svix-timestamp": svix_timestamp,
       "svix-signature": svix_signature,
-    });
+    }) as WebhookEvent;
   } catch (err) {
     return new Response("Bad Request", { status: 400 });
   }
 
-  console.log(msg);
-
+  const evenType = msg.type;
+  if (evenType === "user.created") {
+    const { id, email_addresses, image_url, username } = msg.data;
+    const user = await createUser({
+      clerkId: id,
+      name: username!,
+      username: username!,
+      email: email_addresses[0].email_address,
+      avatar: image_url,
+    });
+    return NextResponse.json({
+      message: "ok",
+      user,
+    });
+  }
   // Rest
 
   return new Response("OK", { status: 200 });
